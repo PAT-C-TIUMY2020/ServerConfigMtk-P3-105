66ad7b9a9ce4ebf5bd57b0de0b4ae6b06439ac22
Riefkha Ananda Putri <riefkhaanandaputri354@gmail.com>
2020-10-19 20:25:45 +0700

    Add project files.

---
+++ ServerConfigMtk-P3-20180140105/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceMtk_P2_20180140105;
+
+namespace ServerConfigMtk_P3_20180140105
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch(Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
